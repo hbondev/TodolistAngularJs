5d32088e36dea46c434d27c7fbc247e8e1dfaff1
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-20 18:00:26 +0330

    feature to check field is not repeated

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,13 @@ let app = angular.module('todo', []);
 
 app.controller('todofunc', ($scope) => {
     $scope.items = [];
+
     $scope.add=function(){
+        if ($scope.userInput && $scope.items.indexOf($scope.userInput) === -1){
         $scope.items.push($scope.userInput);
         $scope.userInput='';
+        }else{
+            alert('این تسک قبلا اضافه شده است!');
+        }
     }
 });
