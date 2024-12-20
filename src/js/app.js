10869b1c23a4dd297718bb4b5599df212753bac9
hosseinbonyadi <hosseinbonyadi2@gmail.com>
2024-12-20 17:52:27 +0330

    feature to add Items

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,9 @@
-let app = angular.module('myApp', []);
+let app = angular.module('todo', []);
 
-app.controller('myController', function($scope) {
-    $scope.items = JSON.parse(localStorage.getItem('items')) || []; 
-
-    $scope.addItem = function() {
-        if ($scope.userInput) {
-            $scope.items.push({ text: $scope.userInput, selected: false });
-            localStorage.setItem('items', JSON.stringify($scope.items));
-            $scope.userInput = '';
-        } else {
-            alert('Please enter some text.'); 
-        }
-    };
-
-    $scope.deleteItem = function(index) {
-        $scope.items.splice(index, 1); 
-        localStorage.setItem('items', JSON.stringify($scope.items));
-    };
-
-    $scope.deleteSelected = function() {
-        $scope.items = $scope.items.filter(item => !item.selected); 
-        localStorage.setItem('items', JSON.stringify($scope.items));
-    };
-
-    $scope.hasSelected = function() {
-        return $scope.items.some(item => item.selected); 
-    };
+app.controller('todofunc', ($scope) => {
+    $scope.items = [];
+    $scope.add=function(){
+        $scope.items.push($scope.userInput);
+        $scope.userInput='';
+    }
 });
-
